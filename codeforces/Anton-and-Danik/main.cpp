5307133b574fab648ce220ea5920aeba7da5b26b
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-11-29 00:34:00 -0500

    {{BACKUP}} Fri Nov 29 00:34:00 2024

---
+++ codeforces/Anton-and-Danik/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+        int n;
+        string s;
+        cin >> n >> s;
+        int a,b;
+        a = 0;
+        b = 0;
+        for (int i = 0; i < s.length(); i++) {
+
+        }
+    }
