7f6e8b7902740aa4d564fe45dcc721e5ae4aba63
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2024-11-29 22:31:18 -0500

    {{BACKUP}} Fri Nov 29 22:31:18 2024

--- README.md
+++ README.md
@@ -4,4 +4,6 @@
 
 [Junior Training Sheet - Competitive Programming](https://docs.google.com/spreadsheets/d/1iJZWP2nS_OB3kCTjq8L6TrJJ4o-5lhxDOyTaocSYc-k/edit?gid=84654839#gid=84654839)
 
+[My Competitive Programming Training Sheet](https://docs.google.com/spreadsheets/d/1QXDzX45hHSb82_gBo1FXZaYpA0x4D3IrWZZrJrqewno/edit?usp=sharing)
+
 [Advent of Code](https://adventofcode.com/)
--- codeforces/Anton-and-Danik/main.cpp
+++ codeforces/Anton-and-Danik/main.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
         int n;
         string s;
         cin >> n >> s;
-        int a,b;
-        a = 0;
-        b = 0;
-        for (int i = 0; i < s.length(); i++) {
-
+        int r;
+        r = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'A')
+                r++;
+            else
+                r--;
         }
+        if (r == 0)
+            cout << "Friendship" << endl;
+        else if (r > 0)
+            cout << "Anton" << endl;
+        else
+            cout << "Danik" << endl;
     }
